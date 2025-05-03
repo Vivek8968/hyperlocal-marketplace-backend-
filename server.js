c9f80f4a09c8a8e78b077d108ed21e4c118baffd
Vivek8968 <98275218+Vivek8968@users.noreply.github.com>
2025-05-03 19:15:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const app = require('./src/app');
+require('dotenv').config();
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Hyperlocal Marketplace backend running on port ${PORT}`);
+});
